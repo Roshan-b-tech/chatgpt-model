0b634d0c30630ce894eaa2a2b1cf7280368c698c
Roshan-b-tech <roshangehlot500@gmail.com>
2025-07-21 20:53:53 +0530

    okkkk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 "use client";
+import React, { ReactNode } from "react";
 import Providers from "./providers";
 import Sidebar from "@/components/Sidebar/Sidebar";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
