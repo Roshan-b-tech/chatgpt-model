ea9db0cbf78af3e83d5b8d58598e6dc0664b966c
Roshan-b-tech <roshangehlot500@gmail.com>
2025-07-21 19:37:09 +0530

    ok

--- src/store/store.ts
+++ src/store/store.ts
@@ -42,5 +42,9 @@ export function makeStore() {
   });
 }
 
+export const store = makeStore();
+
+export default { store };
+
 export type RootState = ReturnType<ReturnType<typeof makeStore>["getState"]>;
 export type AppDispatch = ReturnType<typeof makeStore>["dispatch"];
